1274d2d9b15096ad91e8cecf29f9f2836eadaba5
ranganathnk <ranganathnkulkarni@gmail.com>
2022-11-06 18:12:10 +0530

    connect to database, creating schema

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,29 @@
+---------- Tools required ---------
+    -- MongoDB
+    -- MongoDB Compass
+
+------- How to run after installation --------
+    -- Copy path of MongoDB bin 
+    -- Paste the path in Environment Variables (PATH section)
+    -- Run mongod in command prompt
+    -- Open MongoDB Compass and connect the connection with database
+
+
+------- MongoDB teminologies ---------
+Database -> Database
+Collection -> Table
+Documents -> Rows
+
+
+---- Schema Types ----
+Number
+String
+Boolean
+Date
+Buffer   ---> To store binary data
+ObjectID ---> To assign unique Identifier
+Array
+
 npm i mongoose
 --- Mongoose is a JavaScript object-oriented programming library 
     that creates a connection between MongoDB and the Node.js JavaScript 
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const Joi = require('joi');
-const genres = require('./routes/genres');
-const express = require('express');
-const app = express();
+const mongooseLib = require("mongoose");
+mongooseLib
+  .connect("mongodb://localhost/playground") //Returns Promise
+  .then(() => console.log("Connected to MongoDB...!"))
+  .catch((error) => console.log("Error while connecting", error));
 
-app.use(express.json());
-app.use('/api/genres', genres);
+// Create schema (like table creation in MySql)
+const courseSchema=new mongooseLib.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+
+// const Joi = require('joi');
+// const genres = require('./routes/genres');
+// const express = require('express');
+// const app = express();
+
+// app.use(express.json());
+// app.use('/api/genres', genres);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
