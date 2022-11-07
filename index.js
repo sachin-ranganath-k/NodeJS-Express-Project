f617f0547dfc7a8da4d1b33b464d8113e3382cfd
ranganathnk <ranganathnkulkarni@gmail.com>
2022-11-07 10:39:45 +0530

    mongoose model added

--- index.js
+++ index.js
@@ -5,13 +5,37 @@ mongooseLib
   .catch((error) => console.log("Error while connecting", error));
 
 // Create schema (like table creation in MySql)
-const courseSchema=new mongooseLib.Schema({
+//A Mongoose schema defines the structure of the document, default values, validators, etc
+const courseSchema = new mongooseLib.Schema({
   name: String,
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
-  isPublished: Boolean
+  isPublished: Boolean,
 });
+
+/*Model : Mongoose model provides an interface to the database for creating,/
+querying, updating, deleting records, etc. 
+
+
+Now need to create model for the schema*/
+
+const Course = mongooseLib.model("Course", courseSchema);
+//mongoose.model returns class
+//const Course --> Course is a class
+//model("Course") --> Course is a singular form of collection Courses
+
+//Now need to create instance of class (Object)
+const course = new Course({
+  name: "Node JS",
+  author: "Ranganath K",
+  tags: ["backend", "Javascript"],
+  //Default value set for date. So no need to add date here
+  isPublished: true,
+});
+
+
+
 
 
 
